64a20d6e0c23c7c79915134976121de343fa3c60
artnewbruno <bruno@artnew.com.br>
2022-05-21 09:34:53 +0100

    criação do player e animações

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private CharacterController controller;
+    private Animator animator;
+
+    [Header("Config Player")]
+    public float movementSpeed = 3f;
+
+    private Vector3 direction;
+    private bool isWalk;
+
+    // Start is called before the first frame update
+    void Start() {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Fire1")) {
+            animator.SetTrigger("Attack");
+        }
+
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (direction.magnitude > 0.1f) {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            isWalk = true;
+        } else {
+            isWalk = false;
+        }
+        
+        controller.Move(direction * movementSpeed * Time.deltaTime);
+        animator.SetBool("isWalk", isWalk);
+    }
+}
