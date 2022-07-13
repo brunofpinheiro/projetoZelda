468a99bcd4df1db796a8a9347aa44caa5dad0bff
artnewbruno <bruno@artnew.com.br>
2022-07-13 18:20:54 +0100

    programando o ataque, cortando a grama

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,8 +17,13 @@ public class PlayerController : MonoBehaviour {
 
     [Header("Attack config")]
     public ParticleSystem fxAttack;
-    [SerializeField]
+    public Transform hitBox;
+    [Range(0.2f, 1f)]
+    public float hitRange = 0.5f;
     private bool isAttack;
+    public LayerMask hitMask;
+    public Collider[] hitInfo;
+    public int damageAmount;
 
     // Start is called before the first frame update
     void Start() {
@@ -46,6 +51,12 @@ public class PlayerController : MonoBehaviour {
         isAttack = true;
         animator.SetTrigger("Attack");
         fxAttack.Emit(1);
+
+        hitInfo = Physics.OverlapSphere(hitBox.position, hitRange, hitMask);
+
+        foreach(Collider collider in hitInfo) {
+            collider.gameObject.SendMessage("GetHit", damageAmount, SendMessageOptions.DontRequireReceiver);
+        }
     }
 
     void AttackIsDone() {
@@ -68,5 +79,12 @@ public class PlayerController : MonoBehaviour {
     
     void UpdateAnimator() {
         animator.SetBool("isWalk", isWalk);
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (hitBox != null) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(hitBox.position, hitRange);
+        }
     }
 }
