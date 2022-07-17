52bd6a502038aea1f5e9be6403a5f29a716a1668
artnewbruno <bruno@artnew.com.br>
2022-07-17 15:08:17 +0100

    programando a maquina de estado parte 1

--- Assets/Scripts/SlimeIA.cs
+++ Assets/Scripts/SlimeIA.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeIA : MonoBehaviour
-{
+public class SlimeIA : MonoBehaviour {
     private Animator animator;
     public int hitPoints;
     private bool isDead = false;
     public enemyState state;
 
+    public const float idleWaitTime = 3f;
+    public const float patrolWaitTime = 5f;
+
     // Start is called before the first frame update
     void Start() {
         animator = GetComponent<Animator>();
+        ChangeState(state);
     }
 
     // Update is called once per frame
@@ -62,13 +65,42 @@ public class SlimeIA : MonoBehaviour
     }
 
     void ChangeState(enemyState newState) {
+
+      StopAllCoroutines();
+      state = newState;
+      print(newState);
+
       switch(state) {
         case enemyState.IDLE:
+          StartCoroutine("IDLE");
           break;
 
         case enemyState.ALERT:
           break;
+
+        case enemyState.PATROL:
+          StartCoroutine("PATROL");
+          break;
       }
     }
+
+    IEnumerator IDLE() {
+      yield return new WaitForSeconds(idleWaitTime);
+      if (Rand() < 50) {
+        ChangeState(enemyState.IDLE);
+      } else {
+        ChangeState(enemyState.PATROL);
+      }
+    }
+
+    IEnumerator PATROL() {
+      yield return new WaitForSeconds(patrolWaitTime);
+      ChangeState(enemyState.IDLE);
+    }
+
+    int Rand() {
+      return Random.Range(0, 100); //0...99
+    }
+
     #endregion
 }
